871cc9d2e1c7da0c7f3168c4f632818913896a4e
Matt Farmer <mrfarmer777@gmail.com>
2018-06-28 04:38:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ class App < Sinatra::Base
     erb :index
   end
 
+  post "/" do
+    @pup=Puppy.new(params[:name],params[:breed],params[:age])
+  end
+  
   get "/new" do
     erb :create_puppy
   end
